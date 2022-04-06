39164b003f657123b2a6e29dee75997c436c71b6
adinaaionesei <aionesei.adina22@gmail.com>
2022-04-06 13:28:42 +0300

    mass delete

--- Vendor/Model/ResourceModel/Vendor/Collection.php
+++ Vendor/Model/ResourceModel/Vendor/Collection.php
@@ -2,6 +2,8 @@
 namespace Intern\Vendor\Model\ResourceModel\Vendor;
 class Collection extends \Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection
 {
+    protected $_idFieldName = 'vendor_id';
+
     protected function _construct()
     {
         $this->_init('Intern\Vendor\Model\Vendor', 'Intern\Vendor\Model\ResourceModel\Vendor');
